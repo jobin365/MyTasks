0f815c2161cd4bc621c63c940fe900796ce40b46
Jobin John K <jobinjohnk5@gmail.com>
2022-08-02 18:05:04 +0530

    add email validation

--- client/src/components/RegisterOrLogin.js
+++ client/src/components/RegisterOrLogin.js
@@ -4,15 +4,15 @@ import TextField from "@mui/material/TextField";
 import "./RegisterOrLogin.css";
 import Button from "@mui/material/Button";
 import Axios from "axios";
-import Alert from '@mui/material/Alert';
+import Alert from "@mui/material/Alert";
 import googleLogo from "./images/google.png";
 import LoadingBar from "react-top-loading-bar";
 
 export default function Login(props) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [showAlert,setShowAlert]=useState(false);
-  const [errorMessage,setErrorMessage]=useState("");
+  const [showAlert, setShowAlert] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
 
   function handleUsernameChange(event) {
     setUsername(event.target.value);
@@ -22,34 +22,51 @@ export default function Login(props) {
     setPassword(event.target.value);
   }
 
-  function handleLogin(event){
-    props.load.current.continuousStart();
-    Axios.post("/login",{username:username,password:password}).then((res)=>{
-      if(res.data.login==="success"){
-        props.checkStatus();
-      }else{
-        setErrorMessage(res.data.error);
-        setShowAlert(true);
-      }
-    }).catch((err)=>{
-      setErrorMessage("Check if username or password is correct");
+  function handleLogin(event) {
+    if (isValidEmail(username)) {
+      props.load.current.continuousStart();
+      Axios.post("/login", { username: username, password: password })
+        .then((res) => {
+          if (res.data.login === "success") {
+            props.checkStatus();
+          } else {
+            setErrorMessage(res.data.error);
+            setShowAlert(true);
+          }
+        })
+        .catch((err) => {
+          setErrorMessage("Check if username or password is correct");
+          setShowAlert(true);
+          props.load.current.complete();
+        });
+    } else {
+      setErrorMessage("Invalid Email");
       setShowAlert(true);
-      props.load.current.complete();
-    })
-  
+    }
   }
 
-  function handleRegister(event){
-    props.load.current.continuousStart();
-    Axios.post("/register",{username:username,password:password}).then((res)=>{
-      if(res.data.register==="failed"){
-        setErrorMessage(res.data.error);
-        setShowAlert(true);
-        props.load.current.complete();
-      }else{
-        props.checkStatus();
-      }
-    })
+  function handleRegister(event) {
+    if (isValidEmail(username)) {
+      props.load.current.continuousStart();
+      Axios.post("/register", { username: username, password: password }).then(
+        (res) => {
+          if (res.data.register === "failed") {
+            setErrorMessage(res.data.error);
+            setShowAlert(true);
+            props.load.current.complete();
+          } else {
+            props.checkStatus();
+          }
+        }
+      );
+    } else {
+      setErrorMessage("Invalid Email");
+      setShowAlert(true);
+    }
+  }
+
+  function isValidEmail(email) {
+    return /\S+@\S+\.\S+/.test(email);
   }
 
   return (
@@ -78,13 +95,13 @@ export default function Login(props) {
           justifyContent: "space-evenly",
           alignItems: "center",
           flexWrap: "wrap",
-          marginBottom:"25px"
+          marginBottom: "25px",
         }}
       >
         <TextField
           style={{ marginBottom: "20px" }}
           className="username"
-          label="Username"
+          label="Email"
           value={username}
           onChange={handleUsernameChange}
           variant="outlined"
@@ -100,22 +117,37 @@ export default function Login(props) {
           type="password"
         />
         <div className="lorButtons">
-        <Button className="lorButton" variant="contained" onClick={handleLogin}>
-          Login
-        </Button>
-        <Button className="lorButton" variant="contained" onClick={handleRegister}>
-          Register
-        </Button>
-      </div>
-      <a href="/auth/google" style={{textDecoration:"none"}}>
-      {/* <a href="http://localhost:3001/auth/google" style={{textDecoration:"none"}}> */}
-      <Button variant="outlined" className="googleButton" style={{marginTop:"20px"}}>
-        <img style={{width:"33px",height:"34px",marginRight:"10px"}} src={googleLogo}></img>
-        Login with Google
-      </Button>
-      </a>
+          <Button
+            className="lorButton"
+            variant="contained"
+            onClick={handleLogin}
+          >
+            Login
+          </Button>
+          <Button
+            className="lorButton"
+            variant="contained"
+            onClick={handleRegister}
+          >
+            Register
+          </Button>
+        </div>
+        <a href="/auth/google" style={{ textDecoration: "none" }}>
+          {/* <a href="http://localhost:3001/auth/google" style={{textDecoration:"none"}}> */}
+          <Button
+            variant="outlined"
+            className="googleButton"
+            style={{ marginTop: "20px" }}
+          >
+            <img
+              style={{ width: "33px", height: "34px", marginRight: "10px" }}
+              src={googleLogo}
+            ></img>
+            Login with Google
+          </Button>
+        </a>
       </Paper>
-      {showAlert?<Alert severity="error">{errorMessage}</Alert>:null}
+      {showAlert ? <Alert severity="error">{errorMessage}</Alert> : null}
     </div>
   );
 }
